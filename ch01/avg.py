9bdd1e6fffe5ff6e728747cf289d546adcced45d
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-12-25 18:42:25 +0900

    Add incremental

--- ch01/avg.py
+++ ch01/avg.py
@@ -3,8 +3,12 @@ import numpy as np
 np.random.seed(0)
 rewards = []
 
+Q = 0
+
 for n in range(1, 11):
     reward = np.random.rand()
-    rewards.append(reward)
-    Q = sum(rewards) / n
+    # rewards.append(reward)
+    # Q = sum(rewards) / n
+    Q += (reward - Q) / n
     print(Q)
+
